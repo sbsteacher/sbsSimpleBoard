ce3ceef581c64a088262dcfb04a9a22ee1fd5cb3
ACW <ggomee2323@gmail.com>
2019-11-23 09:46:50 +0900

    비밀번호 단방향 암호화

--- SimpleBoard/src/com/doheum/sb/Utils.java
+++ SimpleBoard/src/com/doheum/sb/Utils.java
@@ -1,14 +1,37 @@
 package com.doheum.sb;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Utils {
-	
+
 	public static int parseStringToInt(String v) {
 		int result = 0;
 		try {
 			result = Integer.parseInt(v);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			result = 0;
 		}
 		return result;
+	}
+
+	public static String encryptSHA256(String str) {
+		String sha = "";
+		try {
+			MessageDigest sh = MessageDigest.getInstance("SHA-256");
+			sh.update(str.getBytes());
+			byte byteData[] = sh.digest();
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			sha = sb.toString();
+
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Encrypt Error - NoSuchAlgorithmException");
+			sha = null;
+		}
+
+		return sha;
 	}
 }
